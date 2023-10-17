85fc6c912110312d6af3d65498cd971d15db12d2
PatriciaMoraru <patriciamoraruv@gmail.com>
2023-10-17 18:52:50 +0300

    Updates on lab no.4

--- text_editor_final_version.c
+++ text_editor_final_version.c
@@ -10,21 +10,38 @@ char buffer[buffer_size_limit] = {0};
 
 void enter_the_text() 
 {
-    char entered_text[buffer_size_limit];
-    int left_space;
-	
-	printf("\n");
-    printf("Enter the text: \n");
+    char entered_text[buffer_size_limit + 2]; 
+    int input_length;
 
-    fgets(entered_text, buffer_size_limit, stdin);
+    printf("\nEnter the text (max %d characters): \n", buffer_size_limit);
 
-    left_space = buffer_size_limit - strlen(buffer) - 1;
+    if (fgets(entered_text, sizeof(entered_text), stdin) == NULL) 
+    {
+        printf("Error reading input.\n");
+        return;
+    }
 
-    strncat(buffer, entered_text, left_space); 
+    input_length = strlen(entered_text); 
+
+    if (entered_text[input_length - 1] == '\n') 
+    {
+        entered_text[input_length - 1] = '\0';
+        input_length--; 
+    } 
+    else if (input_length >= buffer_size_limit + 1) 
+    { 
+        printf("\nLimit exceeded.\n");
+
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF); 
+        entered_text[buffer_size_limit] = '\0';
+        input_length = buffer_size_limit; 
+    }
     
-	printf("\n");
-    printf("The updated text:\n%s\n", buffer);
+    strncpy(buffer, entered_text, input_length + 1); 
+    printf("\nThe text you entered (up to %d characters):\n%s\n", buffer_size_limit, buffer);
 }
+
 
 int separated_word(char c) 
 {
@@ -79,24 +96,44 @@ void word_replacement(char* word, char* substitute)
 
     while ((pos = strstr(pos, word)) != NULL) 
     {
-        int copy_length = pos - last_pos;
-        
-        strncpy(current_pos_in_temp, last_pos, copy_length);
-        current_pos_in_temp = current_pos_in_temp + copy_length;
+        char before = ' ';
+        char after = ' ';
 
-        strcpy(current_pos_in_temp, substitute);
-        current_pos_in_temp = current_pos_in_temp + substitute_length;
+        if (pos != buffer) 
+        {
+            before = *(pos - 1);
+        }
 
-        pos = pos + word_length;
-        last_pos = pos;
+        if (*(pos + word_length) != '\0') 
+        {
+            after = *(pos + word_length);
+        }
+
+        if (separated_word(before) && separated_word(after)) 
+        {
+            int copy_length = pos - last_pos;
+            strncpy(current_pos_in_temp, last_pos, copy_length);
+            current_pos_in_temp += copy_length;
+
+            strcpy(current_pos_in_temp, substitute);
+            current_pos_in_temp += substitute_length;
+
+            pos = pos + word_length;
+            last_pos = pos;
+        }
+        else 
+        {
+            pos = pos + 1;
+        }
     }
-
+    
     strcpy(current_pos_in_temp, last_pos);
     strcpy(buffer, temp_buffer);
 
     printf("\n");
     printf("The updated text:\n%s\n", buffer);
 }
+
 
 void delete_buffer() 
 {
@@ -107,7 +144,7 @@ void delete_buffer()
 int main () 
 {
     char word[word_size_limit], substitute[word_size_limit];
-    int choice;
+    int choice, buffer_current_size;
     
     printf("\nMenu:\n");
     printf("1) Enter Text\n");
@@ -118,34 +155,34 @@ int main ()
 
     while (1) 
 	{
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
-        getchar();
-      
-        switch (choice) 
-		{
-            case 1:
-                enter_the_text();
-                break;
-            case 2:
-                printf("Enter the word for searching:\n");
-                scanf("%s", word);
-                printf("\nNumber of occurrences: %d\n", word_search(word));
-                break;
-            case 3:
-                printf("\nEnter the word for replacement:\n");
-                scanf("%s", word);
-                printf("\nEnter the replacement:\n");
-                scanf("%s", substitute);
-                word_replacement(word, substitute);
-                break;
-            case 4:
-                delete_buffer();
-                break;
-            case 5:
-                return 0;
-            default:
-                printf("Invalid choice\n");
-        }
+	        printf("\nEnter your choice: ");
+	        scanf("%d", &choice);
+	        getchar();
+	      
+	        switch (choice) 
+			{
+	            case 1:
+	                enter_the_text();
+	                break;
+	            case 2:
+	                printf("Enter the word for searching:\n");
+	                scanf("%s", word);
+	                printf("\nNumber of occurrences: %d\n", word_search(word));
+	                break;
+	            case 3:
+	                printf("\nEnter the word for replacement:\n");
+	                scanf("%s", word);
+	                printf("\nEnter the replacement:\n");
+	                scanf("%s", substitute);
+	                word_replacement(word, substitute);
+	                break;
+	            case 4:
+	                delete_buffer();
+	                break;
+	            case 5:
+	                return 0;
+	            default:
+	                printf("Invalid choice\n");
+	        }
     }
 }
